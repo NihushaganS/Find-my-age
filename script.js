0b15bb1df1ab5fe6687c5a7c41677d146c968f08
shagan15 <nihushagans@gmail.com>
2024-01-16 15:58:06 +0530

    validating func added

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ const date = new Date();
 let day = date.getDate();
 let month = 1 + date.getMonth();
 let year = date.getFullYear();
+
+const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+function validate() {
+  const inputs = document.querySelectorAll("input");
+  let validator = true;
+  inputs.forEach((i) => {
+    const parent = i.parentElement;
+    if (!i.value) {
+      i.style.borderColor = "red";
+      parent.querySelector("small").innerText = "this field is required.";
+      validator = false;
+    } else if (monthInp.value > 12) {
+        monthInp.style.borderColor = "red";
+        monthInp.parentElement.querySelector("small").innerText = "must be valid month.";
+        validator = false;
+    } else if (dayInp.value > 31) {
+        dayInp.style.borderColor = "red";
+        dayInp.parentElement.querySelector("small").innerText =
+          "must be valid day.";
+        validator = false;
+    } else {
+      i.style.borderColor = "black";
+      parent.querySelector("small").innerText = "";
+      validator = true;
+    }
+  });
+  return validator;
+}
